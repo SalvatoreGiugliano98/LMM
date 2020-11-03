d69a1387cec2c5ffc4baba4bcc45be6904bcd18e
Salvatore Giugliano <50570222+SalvatoreGiugliano98@users.noreply.github.com>
2020-11-03 09:07:57 +0100

    Aggiunti dei commenti al codice di Contatore a modulo 8

--- Contatore a modulo 8/main.cpp
+++ Contatore a modulo 8/main.cpp
@@ -1,5 +1,7 @@
 #include <stm32f30x.h>
-
+/* crea un contatore da 8 bit che si incremente ogni volta che il tatso User viene premuto (Al rilascio del tasto).
+  Ogni bit del contatore indica un led che deve essere accesso; esempio: 00000001 accende il ledo PE8, 00000011 anncende il led PE8 e il led PE9.
+*/
 void main()
 {
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOEEN; // Attivazione PA e PE
@@ -9,10 +11,10 @@ void main()
   int count = 0;
   
   while(1){
-    if((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0){
-      while((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0){}
-      count++;
-      GPIOE->ODR = (count<<8);
+    if((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0){ //controllo se il stato USER è stato premuto
+      while((GPIOA->IDR & GPIO_IDR_0) == GPIO_IDR_0){} // finquando il tasto il premuto non fa niente
+      count++; // incrementa il contatore una volta che il tasto è rilasciato
+      GPIOE->ODR = (count<<8); //modifico i bit del MODER a seconda del valore di count
     }
   }
   
--- Contatore a modulo 8/settings/Contatore a modulo 8.Debug.cspy.ps1
+++ Contatore a modulo 8/settings/Contatore a modulo 8.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.general.xcl" --backend -f "D:\università\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.general.xcl" --backend -f "C:\Users\salva\Desktop\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\università\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\salva\Desktop\LMM\Contatore a modulo 8\settings\Contatore a modulo 8.Debug.driver.xcl" 
 }
