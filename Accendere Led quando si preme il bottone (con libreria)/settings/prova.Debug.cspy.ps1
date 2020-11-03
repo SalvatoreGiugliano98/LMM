957b900d9175092ba70e00e0d0d6964428494ddf
Salvatore Giugliano <50570222+SalvatoreGiugliano98@users.noreply.github.com>
2020-11-03 09:11:46 +0100

    fix

--- Accendere Led quando si preme il bottone (con libreria)/settings/prova.Debug.cspy.ps1
+++ Accendere Led quando si preme il bottone (con libreria)/settings/prova.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.general.xcl" --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.general.xcl" --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.general.xcl" --debug_file=$debugfile --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Debug.driver.xcl" 
 }
