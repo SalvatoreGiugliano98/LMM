25e32265f1c5f5c0914798ee9c12dde7977d19c5
Salvatore Giugliano <50570222+SalvatoreGiugliano98@users.noreply.github.com>
2020-11-03 09:24:02 +0100

    Creato il programma per resettare la schedina

---
+++ Reset Schedina/main.cpp
@@ -0,0 +1,16 @@
+/*
+  Per resettare la schedina:
+  1. Scollegare la scheda
+  2. Rimuover il jumper JP2 (Sul retro della scheda in alto a destra)
+  3. Collegare il Jumper su BOOT0 e VDD (nella coollonna di pin a destra della schedina, la decima coppia di pin )
+  4. Collegare la schedina
+  5. Eseguire il prgramma sottostante
+  6. Scollegare la scheda
+  7. Rimuover il jumper e rimetterlo du JP2
+*/
+
+
+int main()
+{
+  return 0;
+}
