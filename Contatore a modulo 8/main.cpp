ecf6a71e7d880fcc832b435152535186a95f0697
Salvatore Giugliano <50570222+SalvatoreGiugliano98@users.noreply.github.com>
2020-11-03 10:26:15 +0100

    crezione progetto timer

--- Contatore a modulo 8/main.cpp
+++ Contatore a modulo 8/main.cpp
@@ -2,6 +2,7 @@
 /* crea un contatore da 8 bit che si incremente ogni volta che il tatso User viene premuto (Al rilascio del tasto).
   Ogni bit del contatore indica un led che deve essere accesso; esempio: 00000001 accende il ledo PE8, 00000011 anncende il led PE8 e il led PE9.
 */
+
 void main()
 {
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOEEN; // Attivazione PA e PE
