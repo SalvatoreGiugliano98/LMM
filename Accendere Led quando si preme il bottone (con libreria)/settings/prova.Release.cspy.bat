75594f747aa6c150cf6845b1776026fb17ab9cd5
Salvatore Giugliano <50570222+SalvatoreGiugliano98@users.noreply.github.com>
2020-11-20 10:07:10 +0100

    Progetto Interrupt

--- Accendere Led quando si preme il bottone (con libreria)/settings/prova.Release.cspy.bat
+++ Accendere Led quando si preme il bottone (con libreria)/settings/prova.Release.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" "--debug_file=%~1" --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
 
 @echo off 
 :end
--- Accendere Led quando si preme il bottone (con libreria)/settings/prova.Release.cspy.ps1
+++ Accendere Led quando si preme il bottone (con libreria)/settings/prova.Release.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --debug_file=$debugfile --backend -f "D:\università\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4_2\common\bin\cspybat" -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\salva\Desktop\LMM\Accendere Led quando si preme il bottone (con libreria)\settings\prova.Release.driver.xcl" 
 }
